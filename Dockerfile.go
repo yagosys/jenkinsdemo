d2b99972f23d31db6bfbbc0b50cbd73335893286
EC2 Default User <ec2-user@ip-10-0-0-179.ap-southeast-1.compute.internal>
2021-05-13 10:24:57 +0000

    as

--- Dockerfile.go
+++ Dockerfile.go
@@ -1,9 +1,4 @@
 FROM golang:1.8-alpine AS build-env
-RUN curl -fsSLO https://get.docker.com/builds/Linux/x86_64/docker-17.04.0-ce.tgz \
-  && tar xzvf docker-17.04.0-ce.tgz \
-  && mv docker/docker /usr/local/bin \
-  && rm -r docker docker-17.04.0-ce.tgz
-
 RUN mkdir /go/src/app && apk update && apk add git 
 ADD main.go /go/src/app/
 WORKDIR /go/src/app
